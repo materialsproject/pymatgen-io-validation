617d1179ffd4255321695fbbbef44daefadafd54
matthewkuner <matthewkuner@gmail.com>
2023-10-20 11:07:25 -0700

    modify folder names

---
+++ pymatgen/io/validation/__init__.py
@@ -0,0 +1,5 @@
+"""
+pymatgen-io-validation provides validation tools for inputs and outputs of computational
+simulations and calculations. That is, it checks calculation details against a reference
+to ensure that data is compatible with some standard.
+"""
