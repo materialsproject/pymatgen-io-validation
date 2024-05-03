c5171a8407aeed2fc7f7bcc02a8bd5b0c9a4287b
Matthew Kuner <82329282+matthewkuner@users.noreply.github.com>
2024-05-03 12:00:51 -0700

    Update release.py

--- .github/workflows/release.py
+++ .github/workflows/release.py
@@ -1,9 +1,7 @@
-name: publish new version to PyPI
-
-on: release
-  # release:
-    # types: [published]
-
+name: release
+on:
+  release:
+    types: [published]
 jobs:
   deploy:
     runs-on: ubuntu-latest
@@ -13,23 +11,18 @@ jobs:
     strategy:
       max-parallel: 1
       matrix:
-        package: ["test_validation"]
-
+        package: ["pymatgen-io-validation"]
     steps:
-      - uses: actions/checkout@v4
-
-      - uses: actions/setup-python@v5
+      - uses: actions/checkout@v3
+      - uses: actions/setup-python@v4
         with:
-          python-version: 3.11
-
+          python-version: 3.8
       - name: Install dependencies
         run: |
           python -m pip install --upgrade pip
           pip install setuptools setuptools_scm wheel
-
       - name: Build packages
         run: python setup.py sdist bdist_wheel
-
       - name: Publish package
         uses: pypa/gh-action-pypi-publish@master
         with:
@@ -38,3 +31,48 @@ jobs:
           skip-existing: true
           verbose: true
           packages_dir: dist/
+
+
+
+
+
+# name: publish new version to PyPI
+
+# on: release
+#   # release:
+#     # types: [published]
+
+# jobs:
+#   deploy:
+#     runs-on: ubuntu-latest
+#     permissions:
+#       # For pypi trusted publishing
+#       id-token: write
+#     strategy:
+#       max-parallel: 1
+#       matrix:
+#         package: ["test_validation"]
+
+#     steps:
+#       - uses: actions/checkout@v4
+
+#       - uses: actions/setup-python@v5
+#         with:
+#           python-version: 3.11
+
+#       - name: Install dependencies
+#         run: |
+#           python -m pip install --upgrade pip
+#           pip install setuptools setuptools_scm wheel
+
+#       - name: Build packages
+#         run: python setup.py sdist bdist_wheel
+
+#       - name: Publish package
+#         uses: pypa/gh-action-pypi-publish@master
+#         with:
+#           # user: __token__
+#           # password: ${{ secrets.PYPI_API_TOKEN }}
+#           skip-existing: true
+#           verbose: true
+#           packages_dir: dist/
